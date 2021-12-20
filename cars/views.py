dc31e4848f7041876f1400da42fa582719e508fc
Your Name <you@example.com>
2021-12-20 19:22:07 +0600

    working filter wo path to filter

--- cars/views.py
+++ cars/views.py
@@ -8,21 +8,19 @@ from rest_framework import generics, status
 
 from .models import  Car 
 from .serializer import CarSerializer
-from .filters CarFilterByPriceRange
 
 class CarListView(generics.ListAPIView):
     
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     permission_classes = [AllowAny,]
-    filter_backends = [django_filters.DjangoFilterBackend,]
-    filterset_class = CarFilterByPriceRange
-    
-    # ? как тут реализовать фильтр по цене
-    def get_queryset(self):
-        if [self.request.GET.get('min_price'),self.request.GET.get('max_price')] == [None,None]:    
-            return super().get_queryset(self)            
-        self.filter = self.filterset_class(self.request.GET, queryset=Car.objects.all())
-        return self.filter.qs
 
+    def get_queryset(self):
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
         
+        if [min_price,max_price] == [None,None]:    
+            return super().get_queryset()
+        
+        result_queryset = self.queryset.filter(price__gte=min_price,price__lte=max_price)
+        return result_queryset
