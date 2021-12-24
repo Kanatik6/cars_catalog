8554b68966b11c599bb9262d5f6ae23af90e2e01
Your Name <you@example.com>
2021-12-25 03:48:32 +0600

    update retrieve brand path

--- cars/urls.py
+++ cars/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('list_cars/filter/', CarListView.as_view({'get': 'filter_price'}),name='list_cars_filter_url'),
     path('list_cars/<int:pk>/',CarDetailView.as_view({'get':'list'}),name='cars_detail_url'),
     path('list_brands/',BrandListView.as_view({'get':'list'}),name='list_brand_url'),
-    path('list_brands/<int:pk>/',BrandDetailView.as_view({'get':'list'}),name='detail_brand_url'),
+    path('list_brands/<int:pk>/',BrandDetailView.as_view({'get':'retrieve'}),name='detail_brand_url'),
     path('min_max/',min_max,name='min_max_url')
     ]
