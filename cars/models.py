643e17e7acbf90cb2416fba8d2ec74f3a4d43c13
Your Name <you@example.com>
2021-12-24 14:16:45 +0600

    change brand and year fields

--- cars/models.py
+++ cars/models.py
@@ -5,9 +5,9 @@ class Car(models.Model):
     full_name = models.CharField(max_length=20,default="Не указано")
     price = models.DecimalField(max_digits=15,decimal_places=1,default=0)
     mileage = models.CharField(max_length=20,default='Не указано')
-    breed = models.CharField(max_length=20,default='Не указано')
+    year = models.IntegerField(default=0)
     url = models.URLField(max_length=100)
-    brand = models.CharField(max_length=30)
+    brand = models.ForeignKey('CarBrand',on_delete=models.CASCADE,related_name='cars')
 
     def __str__(self):
         return self.full_name
